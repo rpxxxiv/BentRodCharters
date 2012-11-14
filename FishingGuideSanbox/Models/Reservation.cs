1b029e18b0e7ca8077c89f8a796da94cecdb5767
Andrew Ferrante <rpxxxiv@gmail.com>
2012-11-13 20:55:28 -0800

    BRC Charter Reservation Expanded mock
    Signed-off-by: Andrew Ferrante <rpxxxiv@gmail.com>

--- FishingGuideSanbox/Models/Reservation.cs
+++ FishingGuideSanbox/Models/Reservation.cs
@@ -7,11 +7,7 @@ namespace FishingGuideSanbox.Models
 {
     public class Reservation
     {
-        public int ID { get; set; }
-        public string CustomerName { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
+        public Customer Customer { get; set; }
         public Charter Charter { get; set; }
         public PaymentMethods PaymentMethod { get; set; }
     }
